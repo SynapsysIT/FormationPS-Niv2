f0f0ab8f801f24d01e6b7dc9112611d45ff2b798
Julien Mazoyer <julien@mazoyer.me>
2020-06-08 12:58:39 +0200

    Add Azure Extension

---
+++ Azure/VMExtension.ps1
@@ -0,0 +1,11 @@
+﻿
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
+
+choco install git --params "/NoShellIntegration" -y -r
+choco install vscode -y -r
+choco install vscode-powershell -y -r
+choco install powershell-core -y -r
+choco install microsoft-windows-terminal -y -r
+
+Add-WindowsCapability -Online -Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0"
+Add-WindowsCapability -Online -Name "Rsat.FileServices.Tools~~~~0.0.1.0"
